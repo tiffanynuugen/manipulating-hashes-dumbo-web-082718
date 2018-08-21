dedbb9f09fa51677e918925bfa8d8acb9d75b011
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-21 03:11:33 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,5 +12,14 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
+  def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if{|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
+end
   contact
 end
