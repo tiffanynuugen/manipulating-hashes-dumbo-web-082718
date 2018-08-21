e9c435c3091a9994fb84581ac6053f716945fd96
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-21 03:26:54 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,10 +6,8 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
-  def get_the_values(groceries)
+def get_the_values(groceries)
   groceries.values.flatten
-end#code your solution here!
-
-
-
 end
+
+
